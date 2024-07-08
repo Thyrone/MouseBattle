40792b5b2f2faf9f8aa8531a7b6c7d2b11d556bf
CHERCHOUR Camille <camille.cherchour@brassart.fr>
2024-07-08 17:10:53 +0200

    Condition de victoire
    Trigger en fonction de la position de la main zone par rapport à la largeur de la caméra
    
    apparition d'un texte de victoire
    
    arrêt du "jeu"

--- Assets/Scripts/ZoneManager.cs
+++ Assets/Scripts/ZoneManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 
 public enum Type { Player1, Player2 };
@@ -15,6 +16,12 @@ public class ZoneManager : MonoBehaviour
 
     public float Speed;
 
+    [SerializeField] private float ScreenBorder;
+
+    private Camera _camera;
+
+    public TMP_Text VictoryText;
+
     private void Awake()
     {
         if (instance != null && instance != this)
@@ -28,6 +35,12 @@ public class ZoneManager : MonoBehaviour
         }
         DontDestroyOnLoad(this.gameObject);
 
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        EndGame();
     }
 
     public void Movement(Type ZoneType)
@@ -42,5 +55,24 @@ public class ZoneManager : MonoBehaviour
             ZonePosition.position -= Direction * Speed * Time.deltaTime;
         }
     }
+
+    public void EndGame()
+    {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(ZonePosition.position);
+        if (screenPosition.x > Camera.main.pixelWidth)
+        {
+            Debug.Log("Victoire des bleus !");
+            VictoryText.text = "Victoire des bleus !";
+            Time.timeScale = 0;
+        }
+
+        if (screenPosition.x <= 0)
+        {
+            Debug.Log("Victoire des Jaunes !");
+            VictoryText.text = "Victoire des Jaunes !";
+            Time.timeScale = 0;
+        }
+    }
+
 
 }
